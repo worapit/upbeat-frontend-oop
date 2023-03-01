1694a2e397682707e5521981874b6d60b6c71956
worapit <drm.wmwork@gmail.com>
2023-03-01 17:19:57 +0700

    map page

---
+++ src/Map.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from "react";
+import Hexagon from "./component/Hexagon";
+import CountdownTimer from "./component/CountdownTimer/CountdownTimer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWallet } from "@fortawesome/free-solid-svg-icons";
+import "./cstPlan.css";
+
+export default function CstPlan() {
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <CountdownTimer countdownTimestampMs={Date.now() + 5 * 60 * 1000} />
+          <div className="hex">
+            <Hexagon />
+          </div>
+          <div className="question">
+            <a href="#" style={{ "--clr": "#ff1867", borderRadius: "5000px" }}>
+              <span>?</span>
+              <i></i>
+            </a>
+            <div className="budget">
+              <div className="withIcon">
+                <FontAwesomeIcon icon={faWallet} color="#b19a9a" size="2x" />
+              </div>
+              <span>100000000</span>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper">
+          <h2> CONSTRUCTOR PLAN</h2>
+          <textarea placeholder="plan write here..." required></textarea>
+          <a href="#" style={{ "--clr": "#ff1867" }}>
+            <span style={{ fontFamily: "Bungee" }}>COMPLETE</span>
+            <i></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import CstPlan from "./CstPlan"
-
+import Map from './Map';
 import "bootstrap/dist/css/bootstrap.min.css";
 // import CstPlan from "./CstPlan";
 
