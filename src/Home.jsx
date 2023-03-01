16e248de5f4779cf378348679ec0763a21625713
auswitch <sawit_cha@cmu.ac.th>
2023-03-01 17:04:45 +0700

    edit font game name in home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
+import myCustomFont from '/Users/auswitch/Desktop/upbeat-frontend-oop/src/font/Space.ttf';
 import "./home.css";
+import ReactDOM from 'react-dom';
 
 export default function Home() {
   const lightRef = useRef(null);
@@ -10,32 +12,43 @@ export default function Home() {
     light.style.top = `${e.clientY}px`;
   };
 
+  const styles = `
+    @font-face {
+      font-family: 'gamefont';
+      src: url(${myCustomFont}) format('truetype');
+    }
+  `;
+
   return (
-    <div className="container">
-      <header id="hex-grid" onMouseMove={handleMouseMove}>
-        <div className="light" ref={lightRef}></div>
-        <div className="grid">
-          <div className="button">
-            <p
-              style={{
-                fontFamily: "Bungee",
-                fontSize: "10em",
-                color: "#fff",
-              }}
-            >
-              UPBEAT
-            </p>
-            <a href="#" style={{ "--clr": "#ff1867" }}>
-              <span style={{ fontFamily: "Bungee" }}>START</span>
-              <i></i>
-            </a>
-            <a href="#" style={{ "--clr": "#ff1867"}}>
-              <span style={{ fontFamily: "Bungee" }}>how to play</span>
-              <i></i>
-            </a>
+
+      <div className="container" >
+    
+        <header id="hex-grid" onMouseMove={handleMouseMove}>
+          <div className="light" ref={lightRef}></div>
+          <div className="grid">
+            <div className="button">
+              <p
+                style={{
+                  fontFamily: 'gamefont',
+                  fontSize: "10em",
+                  color: "#fff",
+                }}
+              >
+                UPBEAT
+              </p>
+              <a href="#" style={{ "--clr": "#ff1867" }}>
+                <span style={{ fontFamily: "Bungee" }}>START</span>
+                <i></i>
+              </a>
+              <a href="#" style={{ "--clr": "#ff1867" }}>
+                <span style={{ fontFamily: "Bungee" }}>how to play</span>
+                <i></i>
+              </a>
+            </div>
           </div>
-        </div>
-      </header>
-    </div>
+        </header>
+        <style dangerouslySetInnerHTML={{ __html: styles }} />
+      </div>
+
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CstPlan from "./CstPlan"
-
+//import CstPlan from "./CstPlan"
+import Home from "./Home"
 import "bootstrap/dist/css/bootstrap.min.css";
 // import CstPlan from "./CstPlan";
 
 export default function App() {
   return (
     <div>
-      <CstPlan />
+      <Home />
     </div>
   );
 }
