3cae7e8f165657d73b159dfbb96eada34cb37ea2
worapit <drm.wmwork@gmail.com>
2023-03-04 13:58:23 +0700

    map-budget

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,11 +1,35 @@
 import React, { useRef } from "react";
 import Profile from "./image/profile.png";
 import Hexagon from "./component/Hexagon";
-import "./Map.css"
+import CountdownTimer from "./component/CountdownTimer/CountdownTimer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWallet } from "@fortawesome/free-solid-svg-icons";
+import { faCoins} from "@fortawesome/free-solid-svg-icons";
+import "./Map.css";
 
 export default function Map() {
   return (
-    <div className="backgroundMap ">
+    <div>
+      <div className="time">
+        <span>YOUR TURN</span>
+      </div>
+      <div className="container">
+        <Hexagon />
+      </div>
+      <div className="show-budget">
+        <div className="budget">
+          <div className="withIcon">
+            <FontAwesomeIcon icon={faWallet} color="#b19a9a" size="2x" />
+          </div>
+          <span>100000</span>
+        </div>
+        <div className="budget">
+          <div className="withIcon">
+          <FontAwesomeIcon icon={faCoins} color="#b19a9a" size="2x"/>
+          </div>
+          <span>5000</span>
+        </div>
+      </div>
       <img className="profile" src={Profile} />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Home";
+// import Home from "./Home";
 import CstPlan from "./CstPlan";
+// import Map from "./Map";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-     
+      <Map />
     </div>
   );
 }
