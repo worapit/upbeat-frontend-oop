63126597634b6953403d8b7597d8524c6558a174
worapit <drm.wmwork@gmail.com>
2023-03-06 23:10:24 +0700

    hexagon-input

--- src/component/Hexagon.jsx
+++ src/component/Hexagon.jsx
@@ -1,99 +1,44 @@
 import React from "react";
 import HexW from "../image/hex_white.png";
-import HexB from "../image/hex_blue.png";
-import HexR from "../image/hex_red.png";
 import "./hexagon.css";
 
 export default function Hexagon() {
-  //get turn player array regions deposit
+  //max 16*16
+  //min 9*9
+  const rowcf = 9;
+  const column = 9;
+  const regions = [];
 
-  //configuration file
-  // const row = m;
-  // const column = n;
-  // const regions=[];
-  // const hexagon=[m][n];
-
-  // for(let i = 0; i < row; i++){
-  //   for(let j = 0; j < column; j++){
-
-  //   }
-  // }
-
-  // for (let i = 0; i < column; i++) {
-  //   for (let j = 0; j < row; j++) {}
-  // }
-  // for (let i = 0; i < row; i++) {
-  //   const hexagons = [];
-  //   for (let j = 0; j < column; j++) {
-  //     hexagons.push(
-  //       <div className="column" key={`${i}-${j}`}>
-  //         <img className="img-eye" src={MyImage} />
-  //       </div>
-  //     );
-  //   }
-  //   regions.push(<div className="row">{hexagons}</div>);
-  // }
+  for (let i = 0; i < rowcf; i++) {
+    const row = [];
+    for (let j = 0; j < column; j++) {
+      row.push(HexW);
+    }
+    regions.push(row);
+  }
 
   return (
-    <div className="container">
-      <div className="column">
-        <div className="column1">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-        
+    <div>
+      {regions.map((row, rowIndex) => (
+        <div className="rowcss" key={rowIndex}>
+          {row.map((img, colIndex) =>
+            colIndex % 2 === 0 ? (
+              <img
+                key={`${rowIndex}-${colIndex}`}
+                src={img}
+                className="low"
+                style={{ width: "10%" }}
+              />
+            ) : (
+              <img
+                key={`${rowIndex}-${colIndex}`}
+                src={img}
+                style={{ width: "10%" }}
+              />
+            )
+          )}
         </div>
-        <div className="column2">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-
-        </div>
-        <div className="column1">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-         
-        </div>
-        <div className="column2">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-     
-        </div>
-        <div className="column1">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-       
-        </div>
-        <div className="column2">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-        
-        </div>
-        <div className="column1">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-      
-        </div>
-        <div className="column2">
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-          <img className="img-eye" src={HexW} />
-      
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
