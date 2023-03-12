c195b71c9383e2124f0d1a7b11e1cef44edf0d94
worapit <drm.wmwork@gmail.com>
2023-03-12 14:23:50 +0700

    fix-bg-error

--- src/Map.jsx
+++ src/Map.jsx
@@ -5,6 +5,7 @@ import CstPlan from "./CstPlan";
 import CountdownTimer from "./component/CountdownTimer/CountdownTimer";
 import PopUp from "./component/PopUp";
 
+
 import myCustomFontt from "./font/Space.ttf";
 
 
@@ -38,6 +39,7 @@ export default function Map() {
 
   return (
     <div className="map-page">
+
       <div className="map-time">
         <span
           style={{
