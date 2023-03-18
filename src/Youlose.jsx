fc908ce4ef1e5e9656c3ee7c17f47681e8113fac
worapit <drm.wmwork@gmail.com>
2023-03-18 17:26:51 +0700

    decor-youlose

--- src/Youlose.jsx
+++ src/Youlose.jsx
@@ -3,21 +3,16 @@ import lose from "./image/youlose.mp4";
 import myCustomFontt from "./font/Space.ttf";
 import "./youlose.css";
 
-
-
 export default function Youlose() {
-  
-const styles = `
+  const styles = `
   @font-face {
     font-family: 'space';
     src: url(${myCustomFontt}) format('truetype');
   }
 `;
 
-  
   return (
     <div>
-
       <video
         src={lose}
         autoPlay
@@ -25,25 +20,28 @@ const styles = `
         muted
         play-inline
         id="youlose-video"
-        
       ></video>
+      {/* <div id="yl-popup-container"></div> */}
 
-      
-      {/* <img src={waitwall}></img> */}
-      <div class="youlosecontainer">
-      <div className="youlose-text">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      <h1
-           style={{
-            fontFamily: "space"
-          }}
-        >
-          YOU LOSE</h1>
-  
-      </div>
+      <div className="you-lose-show">
+        <div class="youlosecontainer">
+          <div className="youlose-text">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <h1
+              style={{
+                fontFamily: "space",
+              }}
+            >
+              YOU LOSE
+            </h1>
+          </div>
+        </div>
+
+        <a>helllo</a>
+
       </div>
     </div>
   );
