48ea6a0ebdde5bb664a4018b451635ccbffc7265
auswitch <90751246+auswitch@users.noreply.github.com>
2023-03-27 00:36:22 +0700

    adjust video ulose and uwin

--- src/Youlose.jsx
+++ src/Youlose.jsx
@@ -25,6 +25,41 @@ export default function Youlose() {
     }
   }, [countdown, navigate]);
 
+  useEffect(() => {
+    const videoElement = document.getElementById("youlose-video");
+
+    function resizeVideo() {
+      const windowAspectRatio = window.innerWidth / window.innerHeight;
+      const videoAspectRatio = videoElement.videoWidth / videoElement.videoHeight;
+
+      if (windowAspectRatio > videoAspectRatio) {
+        videoElement.style.width = "100vw";
+        videoElement.style.height = "auto";
+        videoElement.style.top = "50%";
+        videoElement.style.left = "50%";
+        videoElement.style.transform = "translate(-50%, -50%)";
+      } else {
+        videoElement.style.width = "auto";
+        videoElement.style.height = "100vh";
+        videoElement.style.top = "50%";
+        videoElement.style.left = "50%";
+        videoElement.style.transform = "translate(-50%, -50%)";
+      }
+    }
+
+    if (videoElement.readyState >= 2) {
+      resizeVideo();
+    } else {
+      videoElement.addEventListener("loadeddata", resizeVideo);
+    }
+
+    window.addEventListener("resize", resizeVideo);
+
+    return () => {
+      window.removeEventListener("resize", resizeVideo);
+    };
+  }, []);
+
   return (
     <div>
       <video
